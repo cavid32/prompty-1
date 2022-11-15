48c6cb8ad391bf7b82c965dca0a37556867e2693
cavid32 <javidhal@code.edu.az>
2022-11-15 14:59:26 +0400

    first commit

---
+++ Js/main.js
@@ -0,0 +1,8 @@
+function funksiyam(){
+    var age = prompt("yasinizi daxil edin")
+    if(age<18){
+        daxilolma.innerHTML = "GIRISINIZ BLOKLANDI" 
+    }else{
+        daxilolma.innerHTML = "XOS GELMISINIZ"
+    }
+}
